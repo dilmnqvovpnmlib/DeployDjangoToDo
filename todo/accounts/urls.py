811762281122bdd1b562b2eb159afe4cf43cfc02
dilmnqvovpnmlib <simplelpmis6@gmail.com>
2020-05-17 15:51:02 +0900

    fix logic of signup

--- todo/accounts/urls.py
+++ todo/accounts/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('signup/', views.SignUpView.as_view(), name='signup'), # 登録
+    path('signup/', views.SignUpView.as_view(), name='signup'),  # 登録
+    path('', views.AccountsView.as_view(), name='accounts'),
 ]
--- todo/accounts/views.py
+++ todo/accounts/views.py
@@ -1,17 +1,30 @@
-from django.contrib.auth import login
+from django.contrib.auth import authenticate, login
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.views import (
-     PasswordChangeView, PasswordChangeDoneView
-)
-from django.contrib.auth.mixins import LoginRequiredMixin
-from django.http import HttpResponse, HttpResponseRedirect
-from django.shortcuts import render
+from django.http import HttpResponseRedirect
 from django.urls import reverse_lazy
-from django.views.generic import ListView
+from django.views.generic import TemplateView
 from django.views.generic.edit import CreateView
 
 
 class SignUpView(CreateView):
     form_class = UserCreationForm
     template_name = 'accounts/signup.html'
-    success_url = reverse_lazy('login')
+    success_url = reverse_lazy("todo_list")
+
+    def form_valid(self, form):
+        self.object = form.save()
+        username = form.cleaned_data["username"]
+        password = form.cleaned_data["password1"]
+        user = authenticate(
+            self.request,
+            username=username,
+            password=password,
+        )
+        # Signupの処理後にログインの状態にしないと、Loginページに遷移する
+        if user is not None:
+            login(self.request, user)
+        return HttpResponseRedirect(self.get_success_url())
+
+
+class AccountsView(TemplateView):
+    template_name = 'accounts/accounts.html'
