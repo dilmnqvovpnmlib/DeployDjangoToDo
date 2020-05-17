0bb18ed864b104b533e210c749b784ec328860d6
dilmnqvovpnmlib <simplelpmis6@gmail.com>
2020-05-17 15:51:56 +0900

    add bootstrap

--- todo/todo/settings.py
+++ todo/todo/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'accounts.apps.AccountsConfig',
+    'todos.apps.TodosConfig',
 ]
 
 MIDDLEWARE = [
@@ -122,4 +123,4 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-LOGIN_REDIRECT_URL = '/'
+LOGIN_REDIRECT_URL = '/todo/list/'
