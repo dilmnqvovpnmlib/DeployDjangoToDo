74283afb05cfec8210149a5bb3d92aaeaa9f0be9
dilmnqvovpnmlib <simplelpmis6@gmail.com>
2020-05-19 00:23:31 +0900

    flake8

--- todo/todo/views.py
+++ todo/todo/views.py
@@ -1,9 +1,4 @@
-from django.contrib.auth import authenticate, login
-from django.contrib.auth.forms import UserCreationForm
-from django.http import HttpResponseRedirect
-from django.urls import reverse_lazy
 from django.views.generic import TemplateView
-from django.views.generic.edit import CreateView
 
 
 class IndexView(TemplateView):
