fb145de3a9e1f5cf6b637744d5f4e2d80cf358ca
dilmnqvovpnmlib <simplelpmis6@gmail.com>
2020-05-19 21:26:11 +0900

    make docker-compose.yml

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,3 +1,26 @@
+echo Helloooooooooooooooooooooooooooooooooooooooooooooooooooooo
+echo `pwd`
+
+set -e
+
+host="$1"
+shift
+user="$1"
+shift
+password="$1"
+shift
+cmd="$@"
+
+echo "Waiting for mysql"
+until mysql -h"$host" -u"$user" -p"$password" &> /dev/null
+do
+  >&2 echo -n "."
+  sleep 1
+done
+
+>&2 echo "MySQL is up - executing command"
+exec $cmd
+
 python todo/manage.py migrate
 python todo/manage.py collectstatic
 
